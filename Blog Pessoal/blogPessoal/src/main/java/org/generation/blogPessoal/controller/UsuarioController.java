80fe5d8a4cb8639c839605e68cabda174c7b8c09
BartRamandu <bartmelo_93@hotmail.com>
2021-11-17 12:25:33 -0300

    @BartRamandu [Task: Modified Archives]

--- Blog Pessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ Blog Pessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -21,19 +21,16 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@PostMapping("/logar")
-	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user) {
 		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
-	
+
 	@PostMapping("/cadastrar")
-	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.CadastrarUsuario(usuario));
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.CadastrarUsuario(usuario));
 	}
-	
-	
-	
+
 }
